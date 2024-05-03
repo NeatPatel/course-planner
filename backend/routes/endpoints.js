915f160be3a0614052b7cadfdef9e4f77daa5ccc
Noah Kim <noahmk1@uci.edu>
2024-05-03 12:35:56 -0700

    Complete pr/cr parsing function

--- backend/routes/endpoints.js
+++ backend/routes/endpoints.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const fetchCourse = require("../services/api-service.js");
+const evalTokens = require("../services/requisites.js");
 
 // Middleware specific to endpoints
 router.get("/course", async (req, res) => {
@@ -15,18 +17,48 @@ router.get("/course", async (req, res) => {
 });
 
 router.get("/reqs-met", async (req, res) => {
-  let prereqsMet = true;
-  let coreqsMet = true;
-
   const body = req.body;
   const courseId = body["courseId"];
-  const prevCourses = body["prevCourses"];
-  const currCourses = body["currCourses"];
+  let prevCourses = body["prevCourses"];
+  let currCourses = body["currCourses"];
+
+  prevCourses = ["I&C SCI 6B", "I&C SCI 6D", "MATH 2B", "I&C SCI 46"];
+  currCourses = [];
+
+  let gqlData = await fetchRStrings();
+  const prText = gqlData["data"]["course"]["prerequisite_text"];
+  const crText = gqlData["data"]["course"]["corequisite"];
+
+  console.log(courseId);
+  console.log(prevCourses);
+  console.log(currCourses);
 
   res.json({
-    prerequisitesMet: prereqsMet,
-    corequisitesMet: coreqsMet,
+    prerequisitesMet: evalTokens(prText, prevCourses),
+    corequisitesMet: evalTokens(crText, currCourses),
   });
 });
+
+async function fetchRStrings() {
+  try {
+    const response = await axios({
+      url: "https://api.peterportal.org/graphql/",
+      method: "post",
+      data: {
+        query: `
+          query {
+            course(id:"COMPSCI161") {
+                prerequisite_text
+                corequisite
+            }
+          }    
+        `,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return "ERROR: Requisite fetching";
+  }
+}
 
 module.exports = router;
--- backend/services/requisites.js
+++ backend/services/requisites.js
@@ -1,127 +1,40 @@
 const [toTokens, isSyntax] = require("./tokenizer.js");
 const Symbols = require("./symbols.js");
 
-class Node {
-  #value;
-  #left;
-  #right;
-
-  constructor(value) {
-    this.#value = value;
-    this.#left = null;
-    this.#right = null;
-  }
-
-  get value() {
-    return this.#value;
-  }
-  get left() {
-    return this.#left;
-  }
-  get right() {
-    return this.#right;
-  }
-
-  set value(value) {
-    this.#value = value;
-  }
-  set left(n) {
-    this.#left = n;
-  }
-  set right(n) {
-    this.#right = n;
-  }
-}
-
-class RequisiteTree {
-  #head;
-  constructor() {
-    this.#head = null;
-  }
-
-  get head() {
-    return this.#head;
-  }
-
-  set head(n) {
-    this.#head = n;
-  }
-}
-
-/**
- * Returns whether the courses that have been taken satisfy the requirements in the requisite tree.
- * @param {RequisiteTree} prTree Requisite Tree object representing the prerequisites of a course.
- * @param {list[str]} pr List of strings representing all courses that have been taken.
- */
-const checkPrerequisites = (prTree, pr) => {
-  if (!prTree.head) {
-    return true;
-  }
-};
-const checkCorequisites = (crTree, cr) => {
-  if (!crTree.head) {
-    return true;
-  }
-};
-
-const createRequisiteTree = (str) => {
-  const words = toTokens(str);
-
-  let parenDepth = 0;
-  let treeDepth = 0;
-  let rt = new RequisiteTree();
-  let currNode;
-  words.forEach((token) => {
-    currNode = new Node(token);
-    switch (token) {
-      case Symbols.L_PAREN:
-        parenDepth++;
-      case Symbols.R_PAREN:
-        parenDepth--;
-      case Symbols.OR:
-      case Symbols.AND:
-      default:
-        if (!head) {
-          head = new Node(token);
-        }
-    }
-  });
-};
-
 // TESTED
-function _evalTokens(rTokens, coursesTaken) {
-  let newTokens = [];
-  let newItem;
-  rTokens.forEach((token) => {
-    newItem = token;
-    if (!isSyntax(token)) {
-      let taken = coursesTaken.some((course) => {
-        return _equalWithoutSpaces(token, course);
+const evalTokens = (rStr, coursesTaken) => {
+  let currToken;
+  let taken;
+  let rTokens = toTokens(rStr);
+  rLen = rTokens.length;
+
+  for (let i = 0; i < rLen; i++) {
+    currToken = rTokens[i];
+    if (!isSyntax(currToken)) {
+      taken = coursesTaken.some((course) => {
+        return _equalIgnoreCaseSpace(currToken, course);
       });
-      newItem = taken;
+      rTokens[i] = taken;
+    } else if (currToken === Symbols.OR) {
+      rTokens[i] = "||";
+    } else if (currToken === Symbols.AND) {
+      rTokens[i] = "&&";
     }
-    newTokens.push(newItem);
-  });
-  return newTokens;
-}
+  }
+  let boolStr = rTokens.join(" ");
+  return eval(boolStr);
+};
 
 // TESTED
-function _equalWithoutSpaces(input1, input2) {
+function _equalIgnoreCaseSpace(input1, input2) {
   let s1 = input1.replace(/\s+/g, "");
   let s2 = input2.replace(/\s+/g, "");
-  return s1 === s2;
+  return s1.toLowerCase() === s2.toLowerCase();
 }
 
-let tokens = toTokens("( AC ENG 20A OR PLACEMENT EXAM or authorization )");
-let newTokens = _evalTokens(tokens, ["ACENG20A"]);
-console.log(newTokens);
+// let tokens = toTokens("( AC ENG 20A AND PLACEMENT EXAM AND authorization )");
+// let result = evalTokens(tokens, ["ACENG20A", 'PLACEMENTEXAM', 'authorization']);
+// console.log(tokens);
+// console.log("Result: " + result);
 
-// '(',
-// 'AC ENG 20A',
-// 'OR',
-// 'PLACEMENT EXAM',
-// 'OR',
-// 'AUTHORIZATION',
-// ')'
-
-module.exports = createRequisiteTree;
+module.exports = evalTokens;
