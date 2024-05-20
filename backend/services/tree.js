d41903360479135a4521f3cbc32ec24a2b152752
Noah Kim <noahmk1@uci.edu>
2024-05-20 16:22:03 -0700

    Complete tree parsing for prerequisites

--- backend/services/tree.js
+++ backend/services/tree.js
@@ -1,27 +1,9 @@
 // This module handles all operations with tree objects.
 
 const { fetchPRTree } = require("./api-service.js");
+const util = require('util')
 
 
-
-/**
- * 
- * "{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}"
- * 
- * {
- *     \"AND\": [
- *         \"I&C SCI 51\",
- *         \"I&C SCI 6B\",
- *         \"I&C SCI 6D\",
- *         {
- *             \"OR\": [
- *                 \"CSE 43\",
- *                 \"I&C SCI 33\"
- *             ]
- *         }
- *     ]
- * }
- */
 
 /**
  * pr has a     header ("AND" OR "OR")
@@ -35,59 +17,76 @@ const { fetchPRTree } = require("./api-service.js");
 // AP Calculus AB
 
 class Expr {
-    #operator;
-    #items;     // Can either be a course or another expression
+    #pair;    // operation: list
 
-    constructor(operator = null, items = null) {
-        this.#operator = operator;
-        this.#items = items;
+    constructor(pair = null) {
+        this.#pair = null
     }
 
     initFromStr(str) {
-
+        let operation = getOperation(str);
+        let items = getItems(str);
+        let result = {}
+        result[operation] = items
+        this.#pair = result
     }
+
+    get pair() { return this.#pair }
 }
 
 function getOperation(str) {
-    return str.substr(2, str.indexOf(':')-3)
+    return str.substring(2, str.indexOf(':')-1)
 }
 
 function getItems(str) {
-    let itemStr = str.substr(str.indexOf('[')+1, str.lastIndexOf(']')-7)
+    let itemStr = str.substring(str.indexOf('[')+1, str.lastIndexOf(']'))
     const itemStrLen = itemStr.length
     let termStart = 0;
     let items = []
     let i = 0;
     for ( ; i < itemStrLen; i++) {
-        if (itemStr[i] == ',') {
-            items.push(itemStr.substr(termStart, i))
+        console.log('i: ' + i)
+        if (itemStr[i] == ',') {     // If comma, store previous item in list
+            let currItem = itemStr.substring(termStart+1, i-1);
+            console.log(currItem)
+            if (currItem[currItem.length-1] == '}') {
+                let e = new Expr()
+                e.initFromStr(s.substring(0, exprLen))
+                currItem = e.pair
+                console.log(true)
+            }
+            items.push(currItem)
             termStart = i + 1
-        } else {
-            if (i-1 == termStart && itemStr[i] == '{') {
-                // advance to character after the next comma
+        } else {              
+            if (i == termStart && itemStr[i] == '{') {
+                let s = itemStr.substring(i, itemStrLen)
+                let exprLen = getExprLen(s)
+                
+                i += exprLen
             }
         }
     }
-    items.push(itemStr.substr(termStart, i))
+    items.push(itemStr.substring(termStart, i))
     return items
 }
 
 function getExprLen(str) {
-    const strlen = str.length
-    let i = 1;
+    let count = 1;
     let depth = 1;
-    for ( ; depth == 0; i++) {
+    for (let i = 1; depth >= 1; i++) {
         if (str[i] == '{') {
             depth++
         } else if (str[i] == '}') {
             depth--
         }
+        count++;
     }
+    return count;
 }
 
+let e = new Expr()
+e.initFromStr("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\",{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}")
+console.log(e.pair)
 
-let a = getItems("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\", {\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}")
-console.log(a)
-
-
-console.log(getExprLen("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}"))
+let j = JSON.parse("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\",{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}")
+console.log(util.inspect(j, false, null, true /* enable colors */))
