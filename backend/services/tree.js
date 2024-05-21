4a4c66e8bedd215e7e496e8c0c56cc05f7931fd9
Noah Kim <noahmk1@uci.edu>
2024-05-20 17:18:06 -0700

    Complete tree parser

--- backend/services/tree.js
+++ backend/services/tree.js
@@ -1,92 +1,49 @@
 // This module handles all operations with tree objects.
 
-const { fetchPRTree } = require("./api-service.js");
-const util = require('util')
+const convertToTree = (str) => {
+  return JSON.parse(str);
+};
 
-
-
-/**
- * pr has a     header ("AND" OR "OR")
- * also has a   list (courses | tree)
- */
-
-// DOES NOT ACCOUNT FOR ITEMS LIKE:
-// PLACEMENT EXAM or authorizaiton
-// authorization
-// ACT MATH
-// AP Calculus AB
-
-class Expr {
-    #pair;    // operation: list
-
-    constructor(pair = null) {
-        this.#pair = null
+const evalTree = async (treeObj, coursesTaken) => {
+    if (!treeObj) {
+        return true
     }
-
-    initFromStr(str) {
-        let operation = getOperation(str);
-        let items = getItems(str);
-        let result = {}
-        result[operation] = items
-        this.#pair = result
-    }
-
-    get pair() { return this.#pair }
-}
-
-function getOperation(str) {
-    return str.substring(2, str.indexOf(':')-1)
-}
-
-function getItems(str) {
-    let itemStr = str.substring(str.indexOf('[')+1, str.lastIndexOf(']'))
-    const itemStrLen = itemStr.length
-    let termStart = 0;
-    let items = []
-    let i = 0;
-    for ( ; i < itemStrLen; i++) {
-        console.log('i: ' + i)
-        if (itemStr[i] == ',') {     // If comma, store previous item in list
-            let currItem = itemStr.substring(termStart+1, i-1);
-            console.log(currItem)
-            if (currItem[currItem.length-1] == '}') {
-                let e = new Expr()
-                e.initFromStr(s.substring(0, exprLen))
-                currItem = e.pair
-                console.log(true)
-            }
-            items.push(currItem)
-            termStart = i + 1
-        } else {              
-            if (i == termStart && itemStr[i] == '{') {
-                let s = itemStr.substring(i, itemStrLen)
-                let exprLen = getExprLen(s)
-                
-                i += exprLen
-            }
+    const isAnd = 'AND' in treeObj
+    const operator = isAnd ? 'AND' : 'OR';
+    
+    const reqs = treeObj[operator]
+    // and (TRUE): if one is FALSE, return FALSE
+    //  or (FALSE): if one is TRUE , return TRUE
+    const reqLen = reqs.length
+    for (let i = 0; i < reqLen; i++) {
+        let req = reqs[i]
+        let reqTaken;
+        if (typeof req === 'string') {
+            reqTaken = _courseInList(req, coursesTaken)
+        } else {
+            reqTaken = await evalTree(req, coursesTaken)
+        }
+        if (reqTaken != isAnd) {
+            return !isAnd
         }
     }
-    items.push(itemStr.substring(termStart, i))
-    return items
-}
+    return isAnd
+};
 
-function getExprLen(str) {
-    let count = 1;
-    let depth = 1;
-    for (let i = 1; depth >= 1; i++) {
-        if (str[i] == '{') {
-            depth++
-        } else if (str[i] == '}') {
-            depth--
+function _courseInList(course, l) {
+    const listLen = l.length
+    for (let i = 0; i < listLen; i++) {
+        if (_equalIgnoreSpace(course, l[i])) {
+            return true;
         }
-        count++;
     }
-    return count;
+    return false
 }
 
-let e = new Expr()
-e.initFromStr("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\",{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}")
-console.log(e.pair)
+function _equalIgnoreSpace(input1, input2) {
+    let s1 = input1.split(' ').join('')
+    let s2 = input2.split(' ').join('')
+    return s1 === s2
+}
 
-let j = JSON.parse("{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\",{\"AND\":[\"I&C SCI 51\",\"I&C SCI 6B\",\"I&C SCI 6D\",{\"OR\":[\"CSE 43\",\"I&C SCI 33\"]}]}]}]}")
-console.log(util.inspect(j, false, null, true /* enable colors */))
+module.exports = { evalTree, convertToTree }
