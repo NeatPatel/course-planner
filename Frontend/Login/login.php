47f1d7c980371440ef110129a3ec4ee45a50cd79
arrrnav <arnav.chatty@gmail.com>
2024-05-25 14:12:09 -0700

    Added more php functionality

--- Frontend/Login/login.php
+++ Frontend/Login/login.php
@@ -1,4 +1,21 @@
 <?php
- 
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
 
+    $connection = new mysqli('course-eater.clei4w6g4nki.us-west-1.rds.amazonaws.com', 'root', '', 'db', '3306');
+    if ($connection->connect_error) {
+        die('Connection Failed: ' .$connection->connect_error);
+    } else {
+
+        // find the name of the table to insert into, and replace *registration* with that table
+        $hashed_password = $connection->prepare("select password from users where email = (?)");
+        // make sure email/password matches database values
+        if(password_verify($password, $hashed_password)) {
+            // fetch user data
+            // redirect to display page?
+        } else {
+            // 
+        }
+    }
 ?>
--- Frontend/Login/register.php
+++ Frontend/Login/register.php
@@ -1,5 +1,22 @@
 <?php
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $password_confirm = $_POST['password_confirm'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    // $password_confirm = $_POST['password_confirm'];
+
+    $connection = new mysqli('course-eater.clei4w6g4nki.us-west-1.rds.amazonaws.com', 'root', '', 'db', '3306');
+    if ($connection->connect_error) {
+        echo "Something went wrong";
+        die('Connection Failed: ' .$connection->connect_error);
+    } else {
+
+        // find the name of the table to insert into, and replace *registration* with that table
+        // make sure email/password matches database values
+        $statement = $connection->prepare("insert into users(email, hashed_password) values(?, ?)");
+        $statement->bind_param("ss", $email, $hashed_password);
+        $statement->execute();
+        echo "Successfully Registered";
+        $statement->close();
+        $connection->close();
+    }
 ?>
