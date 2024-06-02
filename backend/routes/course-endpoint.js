b8b6674b294aea05cacbcfe91c6838f940bfe352
Noah Kim <noahmk1@uci.edu>
2024-06-01 18:18:03 -0700

    Add endpoint for "/", update course docs + figma

--- backend/routes/course-endpoint.js
+++ backend/routes/course-endpoint.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { fetchCourse } = require("../services/api-service.js");
 
+router.get("/", (req, res) => {
+  res.set('Content-Type', 'text/html')
+  res.send(Buffer.from("<h1>Welcome to the Course Eater API!</h1><p>Please refer to the <a href='https://docs.google.com/document/d/170newpAiWiMHSwhZSjycIg9uimXTCbYeJ-eNbfAKA4k/edit?usp=sharing'>documentation</a>."))
+})
+
 router.get("/course", async (req, res) => {
   try {
     let courseId = req.query.courseId;
